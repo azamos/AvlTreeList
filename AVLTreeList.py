c1202695fa0cd1adf8bf051e222fb0cc3a24c0da
amos zohar <azamoszohar@gmail.com>
2022-12-28 10:11:01 +0200

    rotate

--- AVLTreeList.py
+++ AVLTreeList.py
@@ -22,17 +22,16 @@ class AVLNode(object):
 		self.height = -1 # Balance factor
 		self.rank = 0
 		self.size = 1
+		self.isReal = True
 
-	def isUnrayParentLeft(self):
-		return (not self.left is None) and self.right is None
+	def setToVirtual(self):
+		self.isReal = False
 
-	def isUnaryParentRight(self):
-		return (not self.right is None) and self.left is None
 
 	def isLeft():
 		return self.right is None and self.left is None
 	def isRealNode(self):
-		return not self.value is None
+		return self.isReal
 
 	def isLeftSon(self):
 		return  True if self.parent.left is self else False
@@ -162,6 +161,8 @@ class AVLTreeList(object):
 		self.size = 0
 		self.root = None
 		# add your fields here
+		self.minKeyNode = None
+		self.maxKeyNode = None
 
 
 	"""returns whether the list is empty
@@ -170,8 +171,10 @@ class AVLTreeList(object):
 	@returns: True if the list is empty, False otherwise
 	"""
 	def empty(self):
-		return None
+		return self.root is None
 
+	def isValidIndex(self,index):
+		return False if index < 0 or index > self.size-1 else True
 
 	"""retrieves the value of the i'th item in the list
 
@@ -181,8 +184,23 @@ class AVLTreeList(object):
 	@rtype: str
 	@returns: the the value of the i'th item in the list
 	"""
+
+	""""
+		TODO: TEST
+	"""
 	def retrieve(self, i):
-		return None
+		if self.isValidIndex(i):
+			return None
+		p = self.root
+		while i>=0:
+			if p.rank == i+1:
+				return p
+			elif p.rank > i:
+				p = p.getLeft()
+			elif p.rank < i:
+				p = p.getRight()
+			i = i - p.rank
+		return p
 
 	"""inserts val at position i in the list
 
@@ -194,8 +212,53 @@ class AVLTreeList(object):
 	@rtype: list
 	@returns: the number of rebalancing operation due to AVL rebalancing
 	"""
+	
+
+	""""
+			TODO: TEST
+	"""
 	def insert(self, i, val):
-		return -1
+		newNode = AVLNode(val)
+		
+		leftVirtualKid = AVLNode(None)
+		leftVirtualKid.setToVirtual()
+		leftVirtualKid.setParent(newNode)
+		
+		rightVirtualKid = AVLNode(None)
+		rightVirtualKid.setToVirtual()
+		rightVirtualKid.setParent(newNode)
+		
+		newNode.setLeft(leftVirtualKid)
+		newNode.setRight(rightVirtualKid)
+		
+		newParent = self.traverseTo(i)
+
+	def traverseTo(self,index):
+		traverser = self.root
+		while traverser.isRealNode():
+			if traverser.rank == index:
+				insertLocal = traverser.getParent()
+			if traverser.rank > index:
+				traverser = traverser.getLeft()
+			elif traverser.rank < index:
+				traverser = traverser.getRight()
+		return traverser
+
+	def rotateLeft(self, criminalNode):
+		rightSonOfCriminal = criminalNode.right
+		newRightSonOfProblemNode = rightSonOfCriminal.left
+		parent = criminalNode.parent
+		if criminalNode is parent.right:
+			parent.right = rightSonOfCriminal
+		elif criminalNode is parent.left:
+			parent.left = rightSonOfCriminal
+		criminalNode.parent = rightSonOfCriminal
+		rightSonOfCriminal.parent = parent
+		newRightOfCriminal = rightSonOfCriminal.left
+		rightSonOfCriminal.left = criminalNode
+		criminalNode.right = newRightOfCriminal
+
+
 
 
 	"""deletes the i'th item in the list
@@ -205,6 +268,10 @@ class AVLTreeList(object):
 	@param i: The intended index in the list to be deleted
 	@rtype: int
 	@returns: the number of rebalancing operation due to AVL rebalancing
+	"""
+
+	""""
+			TODO: TEST
 	"""
 	def delete(self, i):
 		return -1
@@ -216,7 +283,7 @@ class AVLTreeList(object):
 	@returns: the value of the first item, None if the list is empty
 	"""
 	def first(self):
-		return None
+		return self.minKeyNode
 
 	"""returns the value of the last item in the list
 
@@ -224,7 +291,7 @@ class AVLTreeList(object):
 	@returns: the value of the last item, None if the list is empty
 	"""
 	def last(self):
-		return None
+		return self.maxKeyNode
 
 	"""returns an array representing list 
 
@@ -232,7 +299,16 @@ class AVLTreeList(object):
 	@returns: a list of strings representing the data structure
 	"""
 	def listToArray(self):
-		return None
+		if self.empty():
+			return None
+		n = self.size
+		arr = [None] * n
+		arr[0] = self.first()
+		arr[n-1] = self.last()
+		for i in range(1,n-1):
+			valAtIndexi = self.retrieve(i)
+			arr[i] = valAtIndexi
+		return arr
 
 	"""returns the size of the list 
 
@@ -240,7 +316,7 @@ class AVLTreeList(object):
 	@returns: the size of the list
 	"""
 	def length(self):
-		return None
+		return self.size
 
 	"""sort the info values of the list
 
@@ -276,7 +352,11 @@ class AVLTreeList(object):
 	@returns: the first index that contains val, -1 if not found.
 	"""
 	def search(self, val):
-		return None
+		arr = self.listToArray()
+		for node in arr:
+			if node.value == val:
+				return node.tank-1
+		return -1
 
 
 
@@ -286,6 +366,6 @@ class AVLTreeList(object):
 	@returns: the root, None if the list is empty
 	"""
 	def getRoot(self):
-		return None
+		return self.root
 
 
